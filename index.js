13b936402a757d9ceaa4e3ddff218547ae3a9573
jsettlem <jsettlem@cs.unc.edu>
2014-10-03 14:34:12 -0400

    Forget heroku, socket.io stuffs. also tts

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var express = require('express')
-var app = express();
+var app = require('express')();
+var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, './', 'index.html'));
+});
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+http.listen(9001, function(){
+  console.log('listening on *:9001');
+});

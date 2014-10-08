55a2f614e28cf84bc5ec2d76e4dbcd39644dfe6f
jsettlem <jsettlem@cs.unc.edu>
2014-10-08 10:55:14 -0400

    First working version

--- index.js
+++ index.js
@@ -3,13 +3,35 @@ var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//files and stuff
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, './', 'index.html'));
 });
 
+app.get('/secret', function(req, res){
+  res.sendFile(path.join(__dirname, './', 'secret.html'));
+});
+
+app.get('/mespeak.js', function(req, res){
+  res.sendFile(path.join(__dirname, './mespeak', 'mespeak.js'));
+});
+
+app.get('/mespeak_config.json', function(req, res){
+  res.sendFile(path.join(__dirname, './mespeak', 'mespeak_config.json'));
+});
+
+app.get('/en-us.json', function(req, res){
+  res.sendFile(path.join(__dirname, './mespeak/voices/en', 'en-us.json'));
+});
+
+app.get('/s.ogg', function(req, res){
+  res.sendFile(path.join(__dirname, './', 's.ogg'));
+});
+
+//socket.io stuff
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('message', function(msg){
+    io.emit("message", msg);
   });
 });
 
